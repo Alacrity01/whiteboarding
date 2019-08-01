367217430995b402b28af3adda0c74e725603f4b
TheAlacrity <brinker.jeff@gmail.com>
2019-08-01 12:25:34 -0500

    added enumerables from hackerrank; is_prime unfinished

---
+++ enumerables_hacker_rank.rb
@@ -0,0 +1,11 @@
+# https://www.hackerrank.com/challenges/ruby-enumerable-introduction/problem
+
+def iterate_colors(colors)
+  # Your code here
+  array = []
+  colors.each do |color|
+    array << color
+  end
+  
+  array
+end
