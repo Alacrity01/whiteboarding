598c1953d0b1087736884c1f77bba9ebe1d085b5
TheAlacrity <brinker.jeff@gmail.com>
2019-08-14 13:51:20 -0500

    completed 6/11 intro functions js

--- js_functions_intro_codecademy.js
+++ js_functions_intro_codecademy.js
@@ -8,6 +8,7 @@ function greetInSpanish() {
   console.log('Buenas Tardes.')
 }
 
+
 // function sayThanks() {
 //   console.log('Thank you for your purchase! We appreciate your business.');
 // }
@@ -27,3 +28,12 @@ function makeShoppingList(item1 = 'milk', item2 = 'bread', item3 = 'eggs'){
   console.log(`Remember to buy ${item2}`);
   console.log(`Remember to buy ${item3}`);
 }
+
+
+function monitorCount(rows, columns) {
+  return rows * columns;
+}
+
+const numOfMonitors = monitorCount(5, 4);
+
+console.log(numOfMonitors);
