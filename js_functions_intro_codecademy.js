da2d036328b9aa725d29019daa54f17c4a938d64
TheAlacrity <brinker.jeff@gmail.com>
2019-08-14 13:37:22 -0500

    added intro to functions from codecademy

---
+++ js_functions_intro_codecademy.js
@@ -0,0 +1,9 @@
+// # From Codecademy (https://www.codecademy.com/courses/introduction-to-javascript/lessons/functions/exercises)
+
+function getReminder() {
+  console.log("Water the plants.");
+}
+
+function greetInSpanish() {
+  console.log('Buenas Tardes.')
+}
