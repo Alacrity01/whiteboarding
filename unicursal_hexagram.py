4f6fe0d9c33f059dc84f4f9dd011153f5b03e46e
TheAlacrity <brinker.jeff@gmail.com>
2019-08-19 13:14:32 -0500

    began setup for turtle state

--- unicursal_hexagram.py
+++ unicursal_hexagram.py
@@ -1,2 +1,20 @@
 # Goal: Write a Turtle program that will create a Unicursal Hexagram, knowing only the interior angle of a hexagon (120 degrees)
 
+import turtle
+
+
+t = turtle.Turtle()
+t.color('white', 'blue')
+t.begin_fill()
+
+
+origin = (-100, 100)
+hexSide = 200
+hexAngle = 120
+
+t.setpos(origin)
+t.ht()
+
+print(t.heading())
+print(t.pos())
+print(t.isvisible())
