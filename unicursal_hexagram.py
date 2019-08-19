fcfc0f6db5204b94162cfd2adb1bcd83ff031e33
TheAlacrity <brinker.jeff@gmail.com>
2019-08-19 13:59:32 -0500

    pointsList added and correctly produces coordinates of 6 points

--- unicursal_hexagram.py
+++ unicursal_hexagram.py
@@ -6,7 +6,7 @@ turtle.color('white', 'blue')
 turtle.bgcolor('black')
 turtle.begin_fill()
 
-origin = (-100, 100)
+origin = (float(-100), float(100))
 hexSide = 200
 hexAngle = 180 - 120
 
@@ -14,17 +14,25 @@ turtle.penup()
 turtle.setpos(origin)
 turtle.ht()
 
-print(turtle.heading())
-print(turtle.pos())
-print(turtle.isvisible())
+# print(turtle.heading())
+# print(turtle.isvisible())
+
+pointsList = [origin]
 
 turtle.pendown()
 turtle.fd(hexSide)
 
-while turtle.pos() != origin:
 
+atOrigin = False
+
+while atOrigin == False:
+    pointsList.append(turtle.pos())
     turtle.right(hexAngle)
     turtle.fd(hexSide)
 
+    if turtle.distance(origin) < 1.0:
+        atOrigin = True
+
+print(pointsList)
 
 turtle.exitonclick()
