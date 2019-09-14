308d7ed291d780f1be0a97fb7c7c6b1b94f1a51b
TheAlacrity <brinker.jeff@gmail.com>
2019-09-13 20:23:14 -0500

    added file for hacker rank easy challenges

--- hackerrank_easy_challenges.rb
+++ hackerrank_easy_challenges.rb
@@ -3,9 +3,14 @@ def solveMeFirst (a, b)
   return a + b
 end
 
-def journeyScheduling(n, roads, journeys)
-    #
-    # Write your code here.
-    #
+# def journeyScheduling(n, roads, journeys)
+#   # n = number of cities
+#   # m = number of possible journeys
+#   # k = total number of cities to be visited
+#   # v = current location
+#   # max = greatest possible distance from current city (v) and another city
+#   # If max returns > 1 result, choose city visited less times. 
+#   # output should be the travelled distance for each journey on a separate line
+  
 
-end
+# end
