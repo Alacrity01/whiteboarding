8dd3ecf0b970cf678adae4c8b9d1d5c0477ac887
TheAlacrity <brinker.jeff@gmail.com>
2019-09-19 16:03:26 -0600

    solution for refactoring using each loop

--- practice_problems.rb
+++ practice_problems.rb
@@ -10,11 +10,15 @@
 #  index = index + 1
 # end
 # p sum
+
+numbers = [1, 2, 4, 2]
+sum = 0
+numbers.each do |number|
+   sum += number
+end
+p sum
+
 # ```
-
-
-
-
 #    1. Write a method called `double_numbers` that takes in an array and returns a new array with each number doubled. Use an `each` loop in the method.
 # ```
 # p double_numbers([4, 1, 3])  #=> [8, 2, 6]
