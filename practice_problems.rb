e70d3cdaa35f10ea97d763ca88671ce1b9101eda
TheAlacrity <brinker.jeff@gmail.com>
2019-09-20 17:35:49 -0600

    added solution using .select with an array of hashes

--- practice_problems.rb
+++ practice_problems.rb
@@ -208,6 +208,14 @@ p numbers.select(&:even?) # or p [1, 2, 4, 2].select(&:even?)
 # ]
 # ```
 # to create a new array of hashes that only contain movies with a rating less than 4.0.
+movies = [
+          {id: 1, title: "Die Hard", rating: 4.0},
+          {id: 2, title: "Bad Boys", rating: 5.0},
+          {id: 3, title: "The Chamber", rating: 3.0},
+          {id: 4, title: "Fracture", rating: 2.0}
+         ]
+
+p flops = movies.select{ |movie| movie[:rating].to_f < 4.0 }.map { |movie| movie }
 
 
 #                                  1. Use the `select` method combined with the `map` method to convert the array of hashes
