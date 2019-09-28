a07730c9abf290f3c9f41d80ef698a3e72c11e56
TheAlacrity <brinker.jeff@gmail.com>
2019-09-28 13:16:03 -0700

    added whiteboarding_1.rb

---
+++ whiteboarding_1.rb
@@ -0,0 +1,32 @@
+# Whiteboarding Questions 1
+
+# Be sure to follow the process outlined in the cheat sheet!
+
+# Easy:
+# 1. Write a method that returns an array of every number from 1 to 100. 
+def array_100
+   array = []
+   
+end
+# 2. Write a method that returns an array of every other number from 1 to 100. (That is, 1, 3, 5, 7 … 99).
+
+# 3. Write a method that returns an array of all numbers from 1 to 1000 that are divisible by 3.
+
+# 4. Write a method that accepts one argument - an array of numbers - and returns an array of all numbers from that original array that are greater than 7. For example, if the input is [5, 8, 1, 7, 9, 10], the function should return [8, 9, 10].
+
+# 5. Write a method that accepts an array of numbers as a parameter, and returns the number of how many 55’s there are in the array. For example, if the input is [55, 4, 7, 55, 9, 1, 55, 2, 3, 55, 0], the output should be 4. NOTE: DO NOT USE RUBY’s built-in “count” method.
+
+# 6. Write a method that accepts an array of numbers and returns the sum of the numbers. For example, if the input is [1, 5, 7, 9, 2, 0], the output should be 24. Don’t use any of the built in “sum” methods that Ruby comes with.
+
+# Medium:
+# 1. Write a method that accepts an array and returns it as a hash. For example, [“a”, “b”, “c] should turn into {0 => “a”, 1 => “b”, 2 => “c”}
+
+# 2. Write a method that accepts a string and returns whether it’s a palindrome. (without using the reverse method)
+
+# Advanced:
+# 1. Write a method to generate/print/store the first "n" prime numbers.
+
+# 2. Given an array of randomly sorted numbers, write a method that sorts them in descending order (without using any sort function built into the language.)
+
+# 3. Given a tic-tac-toe board (matrix of 3 x 3), write a method that can check to see whether X or O won.
+
