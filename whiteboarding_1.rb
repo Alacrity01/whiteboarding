8ac3ad1f3974506169aea6b62c6deba1dc856111
TheAlacrity <brinker.jeff@gmail.com>
2019-09-28 15:10:59 -0700

    added ruby solution for n_prime_numbers method

--- whiteboarding_1.rb
+++ whiteboarding_1.rb
@@ -97,7 +97,7 @@ def count_of_55(arr)
   end  
   count
 end
-p count_of_55(array)
+# p count_of_55(array)
 # 6. Write a method that accepts an array of numbers and returns the sum of the numbers. For example, if the input is [1, 5, 7, 9, 2, 0], the output should be 24. Don’t use any of the built in 'sum' methods that Ruby comes with.
 def sum_the_array(arr)
   arr.reduce(&:+)
@@ -112,7 +112,7 @@ def sum_the_array(arr)
   end
   sum
 end
-p sum_the_array(array)
+# p sum_the_array(array)
 
 
 # Medium:
@@ -127,14 +127,14 @@ def array_to_hash(arr)
   new_hash
 end
 array = ['a', 'b', 'c'] 
-p array_to_hash(array)
+# p array_to_hash(array)
 
 # 2. Write a method that accepts a string and returns whether it’s a palindrome. (without using the reverse method)
 def palindrome?(str)
   str.downcase == str.downcase.reverse
 end
-p palindrome?("Radar")
-p palindrome?("Radars")
+# p palindrome?("Radar")
+# p palindrome?("Radars")
 
 def palindrome?(str)
   reversed_str = ""
@@ -145,10 +145,39 @@ def palindrome?(str)
   end
   str.downcase == reversed_str.downcase
 end
-p palindrome?("Radar")
-p palindrome?("Radars")
+# p palindrome?("Radar")
+# p palindrome?("Radars")
+
 # Advanced:
 # 1. Write a method to generate/print/store the first "n" prime numbers.
+def n_prime_numbers(n)
+  primes = [2]
+  number_to_test = 2
+  while n > primes.length do
+    i = 0
+    number_to_test += 1
+    prime = true
+    primes.length.times do
+      if number_to_test % primes[i] == 0
+        prime = false
+      end
+      i += 1
+    end
+    if prime == true
+      primes << number_to_test
+    end
+  end
+  primes
+end
+
+p n_prime_numbers(10)
+first_10_primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29]
+if n_prime_numbers(10) == first_10_primes
+  print "The method is a success"
+else
+  print "There is an error in the method"
+end
+
 
 # 2. Given an array of randomly sorted numbers, write a method that sorts them in descending order (without using any sort function built into the language.)
 
